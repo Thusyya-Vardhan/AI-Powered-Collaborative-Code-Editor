f1bf4b24c5dd43b79bdf775f5d7719c655956930
Thusyya-Vardhan <thusyya2006@gmail.com>
2026-05-10 17:50:41 +0530

    notes backend - model and CRUD routes

--- Server/index.js
+++ Server/index.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const authRoutes = require('./routes/auth')
 const authMiddleware = require('./middleware/auth')
+const notesRoutes = require('./routes/notes')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/notes', notesRoutes)
 
 mongoose.connect('mongodb://localhost:27017/studyplatform')
   .then(() => console.log('MongoDB connected'))
